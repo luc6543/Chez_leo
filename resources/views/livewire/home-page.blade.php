dc351ba6c9a5b5056e2d5c1e1e639a4a70e79101
jesse <jesse.groenen@student.graafschapcollege.nl>
2024-11-13 15:33:08 +0100

    Icons

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -206,18 +206,18 @@
     <div class="  py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h5 class="section-title ff-secondary text-center text-primary fw-normal">Het Menu</h5>
-                <h1 class="mb-5">Het Menu</h1>
+                <h5 class="section-title ff-secondary text-center text-primary fw-normal">Ons</h5>
+                <h1 class="mb-5">Menu</h1>
             </div>
             <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.1s">
                 <ul class="nav nav-pills d-inline-flex justify-content-center border-bottom mb-5">
                     <li class="nav-item">
                         <a class="d-flex align-items-center text-start mx-3 ms-0 pb-3 active" data-bs-toggle="pill"
                            href="#tab-1">
-                            <i class="fa fa-coffee fa-2x text-primary"></i>
+                            <i class="fa fa-bread-slice fa-2x text-primary"></i>
                             <div class="ps-3">
                                 <small class="text-body">Onze</small>
-                                <h6 class="mt-n1 mb-0">Drankjes</h6>
+                                <h6 class="mt-n1 mb-0">Lunch gerechten</h6>
                             </div>
                         </a>
                     </li>
@@ -226,18 +226,18 @@
                            href="#tab-2">
                             <i class="fa fa-hamburger fa-2x text-primary"></i>
                             <div class="ps-3">
-                                <small class="text-body">Speciale</small>
-                                <h6 class="mt-n1 mb-0">Lunch gerechten</h6>
+                                <small class="text-body">Heerlijke</small>
+                                <h6 class="mt-n1 mb-0">Diner</h6>
                             </div>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="d-flex align-items-center text-start mx-3 me-0 pb-3" data-bs-toggle="pill"
                            href="#tab-3">
-                            <i class="fa fa-utensils fa-2x text-primary"></i>
+                            <i class="fa fa-ice-cream fa-2x text-primary"></i>
                             <div class="ps-3">
-                                <small class="text-body">Heerlijk</small>
-                                <h6 class="mt-n1 mb-0">Diner</h6>
+                                <small class="text-body">Smakelijke</small>
+                                <h6 class="mt-n1 mb-0">Dessert</h6>
                             </div>
                         </a>
                     </li>
