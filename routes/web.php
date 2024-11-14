6fee7222641ad8a942b59bf7464c97682d2b7e8d
jordie <jodie@exapps.nl>
2024-11-14 15:28:55 +0100

    added menuPage

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\HomePage::class);
+Route::get('/menu', \App\Livewire\MenuPage::class);
 
 Route::group(['middleware' => ['auth']], function () {
 
