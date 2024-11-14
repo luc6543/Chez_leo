3ac68a86b78452afb34194917b91a8ec0d69f0f1
jordie <jodie@exapps.nl>
2024-11-14 09:19:03 +0100

    added route group for authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\HomePage::class);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['middleware' => ['auth']], function () {
+
 });
