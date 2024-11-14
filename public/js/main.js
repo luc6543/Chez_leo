bcab29c635be04b1d78b857768a22a10f2e79525
jesse <jesse.groenen@student.graafschapcollege.nl>
2024-11-14 15:31:14 +0100

    Reviews update 2

--- public/js/main.js
+++ public/js/main.js
@@ -89,24 +89,25 @@ $(document).ready(function ($) {
         });
     });
 
-    initializeCarousel();
-
-    // Function to initialize Owl Carousel
-    function initializeCarousel() {
-        $(".testimonial-carousel").owlCarousel({
-            autoplay: true,
-            smartSpeed: 1000,
-            center: true,
-            margin: 24,
-            dots: true,
-            loop: true,
-            nav: false,
-            responsive: {
-                0: { items: 1 },
-                768: { items: 2 },
-                992: { items: 3 },
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
             },
-        });
-    }
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            },
+        },
+    });
 
 })(jQuery);
