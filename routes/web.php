a5b0bc0260477a19027d69aa9912b50862381f55
Floris Halleriet <floris.halleriet@student.graafschapcollege.nl>
2024-11-15 11:32:50 +0100

    feat: allow admins to get into the reservation page as well

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\HomePage::class);
-Route::get('/reservations', \App\Livewire\ReservationPage::class)->middleware('role:medewerker');
+Route::get('/reservations', \App\Livewire\ReservationPage::class)->middleware('role:medewerker|admin');
 Route::get('/menu', \App\Livewire\MenuPage::class);
 
 Route::get('/over-ons', \App\Livewire\OverOnsPage::class);
