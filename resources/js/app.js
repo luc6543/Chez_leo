57159151463ce2f20a2afc325d45bf5972729da1
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-11-15 09:25:29 +0100

    flatpicker out of app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
-import './bootstrap';
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.css";
+import "./bootstrap";
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.css";
