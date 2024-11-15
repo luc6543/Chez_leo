c15ec9c0d1909fd3c6a37013f3679117d204760f
jordie <jodie@exapps.nl>
2024-11-15 11:21:23 +0100

    changed userspage

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ Route::get('/menu', \App\Livewire\MenuPage::class);
 Route::get('/over-ons', \App\Livewire\OverOnsPage::class);
 Route::get('/menu', \App\Livewire\MenuPage::class);
 
+Route::get('/over-ons', \App\Livewire\OverOnsPage::class);
+
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/admin/users', \App\Livewire\UsersPage::class)->middleware('role:admin');
+    Route::get('/reservations', \App\Livewire\ReservationPage::class)->middleware('role:medewerker');
 });
