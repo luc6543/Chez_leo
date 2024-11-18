ccfa120429d93b914c40bb2e554fa52cd63122f6
jordie <jodie@exapps.nl>
2024-11-18 11:59:06 +0100

    small margin added to footer.

--- resources/views/livewire/footer-page.blade.php
+++ resources/views/livewire/footer-page.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="mt-5">
     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
