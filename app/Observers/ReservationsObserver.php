8243bf9a3523285e67177935cf97d21c17d0cb50
jordie <jodie@exapps.nl>
2024-11-18 13:47:24 +0100

    add reservationsObserver

---
+++ app/Observers/ReservationsObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Reservation;
+
+class ReservationsObserver
+{
+    /**
+     * Handle the Reservation "created" event.
+     */
+    public function created(Reservation $reservation): void
+    {
+        $reservation->bill()->create([
+            'reservation_id' => $reservation->id,
+            'user_id' => $reservation->user->id,
+            'table_id' => $reservation->table->id
+            ]);
+    }
+
+    /**
+     * Handle the Reservation "updated" event.
+     */
+    public function updated(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "deleted" event.
+     */
+    public function deleted(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "restored" event.
+     */
+    public function restored(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "force deleted" event.
+     */
+    public function forceDeleted(Reservation $reservation): void
+    {
+        //
+    }
+}
