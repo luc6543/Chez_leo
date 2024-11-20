20e46916b8f205f0a2b73353e310fe2d95e8da73
jordie <jodie@exapps.nl>
2024-11-20 09:23:11 +0100

    changed bill styling

--- resources/views/livewire/bill-detail-page.blade.php
+++ resources/views/livewire/bill-detail-page.blade.php
@@ -1,16 +1,22 @@
-<div class="mt-20 p-4 flex gap-10 justify-center">
-    <div class="mt-10 ml-5">
-        <a href="/profile"class="" ><i class=" fa fa-3x fa-arrow-left"></i></a>
+<div class="mt-20 p-4 flex flex-col lg:flex-row gap-2 lg:gap-10 items-center lg:items-stat lg:justify-center">
+    <div class="rounded-full flex justify-center items-center p-2 bg-[#11172b] shadow">
+        <a href="/profile" class="" ><i class=" fa fa-3x fa-arrow-left"></i></a>
     </div>
-    <div class="bg-white flex flex-col w-3/4 lg:w-1/5 gap-5 p-4 bg-no-repeat bg-cover shadow bg-[url('https://img.freepik.com/free-photo/white-crumpled-paper-background-simple-diy-craft_53876-128183.jpg?semt=ais_hybrid')]">
+    <div class="bg-white flex flex-col w-3/4 lg:w-fit gap-5 p-4 bg-no-repeat bg-cover shadow bg-[url('https://img.freepik.com/free-photo/white-crumpled-paper-background-simple-diy-craft_53876-128183.jpg?semt=ais_hybrid')]">
         <div>
             <i class="fa fa-utensils me-3"></i>
             Chez Leo
         </div>
-        <span>Start datum: {{$bill->reservation->start_time }}</span>
-        <span>Eind datum: {{$bill->reservation->end_time}}</span>
+        <div class="flex justify-between gap-5">
+            <span>Start datum:</span>
+            <span>{{$bill->reservation->start_time }}</span>
+        </div>
+        <div class="flex justify-between gap-5">
+            <span>Eind datum:</span>
+            <span>{{$bill->reservation->end_time}}</span>
+        </div>
         @foreach($bill->products as $product)
-            <div class="flex justify-between">
+            <div class="flex justify-between gap-5">
                 <span class="">{{$product->dish_name}} {{ $product->pivot->quantity }}x {{$product->price}}</span>
                 <span class="">€ {{ number_format($product->pivot->quantity * $product->price,2) }}</span>
             </div>
