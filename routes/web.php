6c073805a369004754cd344c7413904a5fe49c89
Floris Halleriet <floris.halleriet@student.graafschapcollege.nl>
2024-11-21 13:57:40 +0100

    fix: remove some useless routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\HomePage::class);
-Route::get('/tables', \App\Livewire\TablePage::class)->middleware('role:medewerker|admin');
-Route::get('/reservations', \App\Livewire\ReservationPage::class)->middleware('role:medewerker|admin');
 Route::get('/menu', \App\Livewire\MenuPage::class);
 
 Route::get('/over-ons', \App\Livewire\OverOnsPage::class);
@@ -12,10 +10,11 @@ Route::get('/recenties', \App\Livewire\RecentiesPage::class);
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/admin/reservations', \App\Livewire\ReservationPage::class)->middleware('role:medewerker');
+    Route::get('/admin/tables', \App\Livewire\TablePage::class)->middleware('role:medewerker|admin');
     Route::get('/admin/users', \App\Livewire\UsersPage::class)->middleware('role:admin');
-    Route::get('/profile', \App\Livewire\ProfilePage::class);
-    Route::get('admin/order', \App\Livewire\OrderPage::class)->middleware('role:medewerker');
-    Route::get('admin/reservation/{reservation}', \App\Livewire\TableOrderPage::class)->middleware('role:medewerker');
+    Route::get('/admin/reservation/{reservation}', \App\Livewire\TableOrderPage::class)->middleware('role:medewerker');
+    Route::get('/admin/order', \App\Livewire\OrderPage::class)->middleware('role:medewerker');
     Route::get('/recenties/toevoegen', \App\Livewire\RecentiesToevoegPage::class);
-    Route::get('/bill/{bill}',\App\Livewire\BillDetailPage::class);
+    Route::get('/bill/{bill}', \App\Livewire\BillDetailPage::class);
+    Route::get('/profile', \App\Livewire\ProfilePage::class);
 });
