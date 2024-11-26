fd61d6aec7649fbf43d4796150e5d39c9933b0d5
jordie <jodie@exapps.nl>
2024-11-26 13:06:19 +0100

    changed styling added link to navbar

--- resources/views/livewire/kitchen-manager.blade.php
+++ resources/views/livewire/kitchen-manager.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-28 bg-white mb-16">
     <div class="ml-6 flex justify-center">
         <div class="flex space-x-1 rounded-lg bg-slate-100 p-0.5">
-            <button wire:click="changeMode('kitchen')" class="flex items-center rounded-md py-[0.4375rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 @if($mode == 'kitchen') bg-white @endif shadow" id="headlessui-tabs-tab-:r8q:" role="tab" type="button" aria-selected="true" tabindex="0">
+            <button wire:click="changeMode('kitchen')" class="flex items-center rounded-md py-[0.4375rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 @if($mode == 'kitchen') bg-white @endif " id="headlessui-tabs-tab-:r8q:" role="tab" type="button" aria-selected="true" tabindex="0">
                 <i class="fa fa-utensils me-3"></i><span class="sr-only lg:not-sr-only lg:ml-2 text-slate-900">Keuken</span>
             </button>
             <button wire:click="changeMode('bar')" class="flex items-center rounded-md py-[0.4375rem] pl-2 pr-2 @if($mode == 'bar') bg-white @endif text-sm font-semibold lg:pr-3" id="headlessui-tabs-tab-:r8s:" role="tab" type="button" aria-selected="false" tabindex="-1">
