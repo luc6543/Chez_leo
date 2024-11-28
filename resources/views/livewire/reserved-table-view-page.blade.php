0f0bdd96b1117bc81c258afa6a0cfc2f0e266f95
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-11-28 13:40:10 +0100

    tafel reservering styling

--- resources/views/livewire/reserved-table-view-page.blade.php
+++ resources/views/livewire/reserved-table-view-page.blade.php
@@ -29,22 +29,22 @@
                                 <div class="bg-white p-4 rounded shadow">
                                     <h2 class="text-xl font-semibold">Tafel {{ $table->table_number }}</h2>
                                     <p>{{ $table->chairs }} stoelen</p>
-                                    <ul>
+                                    <div class="grid grid-cols-2 gap-7 items-center relative">
                                         @foreach($tableReservations as $reservation)
-                                            <h3 class="text-lg font-semibold mt-2">Reservering {{ $reservation->id }}</h3>
-                                            <li>
-                                                <p>Van {{ Carbon::parse($reservation->start_time)->format('H:i') }} tot
-                                                    {{ Carbon::parse($reservation->end_time)->format('H:i') }}
-                                                </p>
-                                                @if ($reservation->user || $reservation->guest_name)
-                                                    <p>Klant: {{ $reservation->user->name ?? $reservation->guest_name }}</p>
-                                                @endif
-                                                <p>{{ $reservation->people }}
-                                                    {{ $reservation->people == 1 ? 'persoon' : 'personen' }}
-                                                </p>
-                                            </li>
+                                            <div class="relative flex flex-col items-center justify-center">
+                                                <!-- Reservation Content -->
+                                                <div class="text-center">
+                                                    <p class="text-lg font-semibold">
+                                                        {{ Carbon::parse($reservation->start_time)->format('H:i') }} -
+                                                        {{ Carbon::parse($reservation->end_time)->format('H:i') }}</p>
+                                                    @if ($reservation->user || $reservation->guest_name)
+                                                        <p>{{ $reservation->user->name ?? $reservation->guest_name }}</p>
+                                                    @endif
+                                                    <p>{{ $reservation->people }} {{ $reservation->people == 1 ? 'persoon' : 'personen' }}</p>
+                                                </div>
+                                            </div>
                                         @endforeach
-                                    </ul>
+                                    </div>
                                 </div>
                             @endif
                     @endforeach
