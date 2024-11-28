cd86ea1ad94b198b300901fc2645fcda1b3deefb
Floris Halleriet <floris.halleriet@student.graafschapcollege.nl>
2024-11-28 15:04:47 +0100

    feat: add some really nice borders to seperate the reservations

--- resources/views/livewire/reserved-table-view-page.blade.php
+++ resources/views/livewire/reserved-table-view-page.blade.php
@@ -17,35 +17,37 @@
                     <h2 class="text-xl font-semibold">Geen reserveringen voor deze dag</h2>
                 </div>
             @else
-                @foreach($tables as $table)
-                        @php
-                            $tableReservations = $table->reservations->filter(function ($reservation) use ($date_time) {
-                                $date = Carbon::parse($date_time)->format('Y-m-d');
-                                return Carbon::parse($reservation->start_time)->format('Y-m-d') <= $date &&
-                                    Carbon::parse($reservation->end_time)->format('Y-m-d') >= $date;
-                            })->sortBy('start_time');
-                        @endphp
-                        @if($tableReservations->isNotEmpty())
-                            <div class="bg-white p-4 rounded shadow">
-                                <h2 class="text-xl font-semibold">Tafel {{ $table->table_number }}</h2>
-                                <p>{{ $table->chairs }} stoelen</p>
-                                <div class="grid grid-cols-2 gap-7 items-center relative">
-                                    @foreach($tableReservations as $reservation)
-                                        <div class="relative flex flex-col items-center justify-center">
-                                            <!-- Reservation Content -->
-                                            <div class="text-center">
-                                                <p class="text-lg font-semibold">{{ Carbon::parse($reservation->start_time)->format('H:i') }} - {{ Carbon::parse($reservation->end_time)->format('H:i') }}</p>
-                                                @if ($reservation->user || $reservation->guest_name)
-                                                    <p>{{ $reservation->user->name ?? $reservation->guest_name }}</p>
-                                                @endif
-                                                <p>{{ $reservation->people }} {{ $reservation->people == 1 ? 'persoon' : 'personen' }}</p>
+                    @foreach($tables as $table)
+                            @php
+                                $tableReservations = $table->reservations->filter(function ($reservation) use ($date_time) {
+                                    $date = Carbon::parse($date_time)->format('Y-m-d');
+                                    return Carbon::parse($reservation->start_time)->format('Y-m-d') <= $date &&
+                                        Carbon::parse($reservation->end_time)->format('Y-m-d') >= $date;
+                                })->sortBy('start_time');
+                            @endphp
+                            @if($tableReservations->isNotEmpty())
+                                <div class="bg-white p-4 rounded shadow">
+                                    <h2 class="text-xl font-semibold">Tafel {{ $table->table_number }}</h2>
+                                    <p>{{ $table->chairs }} stoelen</p>
+                                    <div class="grid grid-cols-2 items-center relative">
+                                        @foreach($tableReservations as $reservation)
+                                            <div class="relative flex flex-col items-center justify-center {{ in_array($loop->iteration, [2, 4]) ? 'border-l-2 border-gray-500 h-2/3' : '' }}">
+                                                <!-- Reservation Content -->
+                                                <div class="text-center w-3/4 pt-2 {{ in_array($loop->iteration, [3, 4, 5]) ? 'border-t-2 border-gray-500' : '' }}">
+                                                    <p class="text-lg font-semibold">
+                                                        {{ Carbon::parse($reservation->start_time)->format('H:i') }} -
+                                                        {{ Carbon::parse($reservation->end_time)->format('H:i') }}</p>
+                                                    @if ($reservation->user || $reservation->guest_name)
+                                                        <p>{{ $reservation->user->name ?? $reservation->guest_name }}</p>
+                                                    @endif
+                                                    <p>{{ $reservation->people }} {{ $reservation->people == 1 ? 'persoon' : 'personen' }}</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    @endforeach
-                                </div>                                    
-                            </div>
-                        @endif
-                @endforeach
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endif
+                    @endforeach
             @endif
         </div>
     </div>
