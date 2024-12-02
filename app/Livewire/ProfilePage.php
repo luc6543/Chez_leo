5df05aff9e5486403b95a17604d8aa44e51ab660
jesse <jesse.groenen@student.graafschapcollege.nl>
2024-12-02 15:18:40 +0100

    After review has been deleted, page refreshes.

--- app/Livewire/ProfilePage.php
+++ app/Livewire/ProfilePage.php
@@ -147,6 +147,7 @@ class ProfilePage extends Component
         if ($review) {
             $review->delete();
             session()->flash('message', 'Recensie succesvol verwijderd!');
+            return redirect('/profile');
         } else {
             session()->flash('error', 'Geen toegang om deze recensie te verwijderen.');
         }
